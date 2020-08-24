e8892a22bc278ba80c4da36dd2c9ceddaaaf3ac4
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-08-24 18:57:01 +0300

    added filter by type and states

--- app/repository/LeadRepository.js
+++ app/repository/LeadRepository.js
@@ -2,10 +2,14 @@
 const db = require('../../database/models');
 
 const LeadRepository = {
-    getAll() {
+    getAll(type, states) {
         return new Promise(async (resolve, reject) => {
 
-            let data = await db.sequelize.query('SELECT leads.id, users.fname, users.lname, leads.email, leads.property, status.name AS status, states.name AS state, prices.price, leads.createdAt as created FROM leads INNER JOIN users ON leads.user_id = users.id INNER JOIN status ON leads.status_id = status.id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id', {
+            if (states) {
+                let statesQuery = `and leads.state_id in ((SELECT states.id FROM states WHERE states.name = ${states.fs}), (SELECT states.id FROM states WHERE states.name = ${states.ls}))`;
+            }
+
+            let data = await db.sequelize.query(`SELECT leads.id, users.fname, users.lname, leads.email, leads.property, STATUS .name AS STATUS , states.name AS state, prices.price, leads.createdAt AS created FROM leads INNER JOIN users ON leads.user_id = users.id INNER JOIN STATUS ON leads.status_id = STATUS .id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id WHERE leads.type_id = (SELECT types.id FROM types WHERE types.name = ${type}) ` + states ? statesQuery : '', {
                 type: db.sequelize.QueryTypes.SELECT,
             }).catch((e) => {
                 console.error(e);
