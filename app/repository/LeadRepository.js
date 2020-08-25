3fe182de5173e6fe1fe21e19fdc8c28d398da4a7
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-08-25 12:25:51 +0300

    Fixed issue with user

--- app/repository/LeadRepository.js
+++ app/repository/LeadRepository.js
@@ -10,7 +10,7 @@ const LeadRepository = {
                 statesQuery = ` and leads.state_id in ((SELECT states.id FROM states WHERE states.name = '${states.fs}'), (SELECT states.id FROM states WHERE states.name = '${states.ls}'))`;
             }
 
-            let data = await db.sequelize.query(`SELECT leads.id, users.fname, users.lname, leads.email, leads.property, STATUS .name AS STATUS , states.name AS state, prices.price, leads.createdAt AS created FROM leads INNER JOIN users ON leads.user_id = users.id INNER JOIN STATUS ON leads.status_id = STATUS .id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id WHERE leads.type_id = (SELECT types.id FROM types WHERE types.name = '${type}')` + statesQuery, {
+            let data = await db.sequelize.query(`SELECT leads.id, users.fname, users.lname, leads.email, leads.property, status.name AS status , states.name AS state, prices.price, leads.createdAt AS created FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN status ON leads.status_id = status .id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id WHERE leads.type_id = (SELECT types.id FROM types WHERE types.name = '${type}')` + statesQuery, {
                 type: db.sequelize.QueryTypes.SELECT,
             }).catch((e) => {
                 console.error(e);
@@ -28,7 +28,10 @@ const LeadRepository = {
     getOne(id) {
         return new Promise(async (resolve, reject) => {
 
-            const data = await db.sequelize.query('SELECT leads.id, users.fname, users.lname, leads.email, leads.property, status.name AS status , states.name AS state, prices.price, leads.createdAt AS created FROM leads INNER JOIN users ON leads.user_id = users.id INNER JOIN STATUS ON leads.status_id = STATUS.id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id WHERE leads.id = ' + id, {
+            "INNER JOIN users ON leads.user_id = users.id"
+        "users.fname, users.lname,"
+
+            const data = await db.sequelize.query('SELECT leads.id, users.fname, users.lname, leads.email, leads.property, status.name AS status , states.name AS state, prices.price, leads.createdAt AS created FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN STATUS ON leads.status_id = STATUS.id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id WHERE leads.id = ' + id, {
                 type: db.sequelize.QueryTypes.SELECT,
             }).catch((e) => {
                 console.error(e);
