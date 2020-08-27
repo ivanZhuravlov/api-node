4165649cea93e9548b75ef408a77fdb4605c974d
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-08-27 20:26:04 +0300

    removed midd.

--- routes/call.js
+++ routes/call.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const { authenticateToken } = require('../app/middleware/authMiddleware');
 const { token, voice } = require('../app/controller/callController');
 
-router.get('/token', authenticateToken, token);
+router.get('/token', token);
 
-router.post('/voice', authenticateToken, voice);
+router.post('/voice', voice);
 
 module.exports = router;
--- routes/record.js
+++ routes/record.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { authenticateToken } = require('../app/middleware/authMiddleware');
 const { fetchRecordsFromTwilioAndSaveToDB } = require('../app/controller/recordController');
 
-router.post('/fetch', authenticateToken, fetchRecordsFromTwilioAndSaveToDB);
+router.post('/fetch', fetchRecordsFromTwilioAndSaveToDB);
 
 module.exports = router;
