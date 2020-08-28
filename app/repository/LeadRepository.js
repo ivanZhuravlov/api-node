2d127da3b3f05a70c0403690f37d9eadc8d78ece
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-08-28 16:21:22 +0300

    Fixed issue with status

--- app/repository/LeadRepository.js
+++ app/repository/LeadRepository.js
@@ -17,7 +17,7 @@ const LeadRepository = {
                 statesQuery = ` and leads.state_id in (${stringOfStatesIds})`;
             }
 
-            let data = await db.sequelize.query(`SELECT leads.id, leads.fullname, users.fname, users.lname, leads.email, leads.property, status.name AS status , states.name AS state, prices.price, leads.createdAt AS created FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN status ON leads.status_id = status .id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id WHERE leads.type_id = (SELECT types.id FROM types WHERE types.name = '${type}')` + statesQuery, {
+            let data = await db.sequelize.query(`SELECT leads.id, leads.fullname, users.fname, users.lname, leads.email, leads.property, status.title AS status, states.name AS state, prices.price, leads.createdAt AS created FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN status ON leads.status_id = status .id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id WHERE leads.type_id = (SELECT types.id FROM types WHERE types.name = '${type}')` + statesQuery, {
                 type: db.sequelize.QueryTypes.SELECT,
             }).catch((e) => {
                 console.error(e);
