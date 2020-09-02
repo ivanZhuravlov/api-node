122158c41c02df3f6071e7f46a757f9454465bcd
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-09-02 15:47:51 +0300

    Fixed issue

--- app/controller/callController.js
+++ app/controller/callController.js
@@ -23,9 +23,9 @@ function voice(req, res) {
     const voiceResponse = new VoiceResponse();
 
     voiceResponse.dial({
-        recordingStatusCallback: 'https://call.leads.joinblueberry.com/api/call/record-callback',
-        recordingStatusCallbackEvent="completed",
         record: 'record-from-answer-dual',
+        recordingStatusCallbackEvent: "completed",
+        recordingStatusCallback: 'https://call.leads.joinblueberry.com/api/call/record-callback',
         callerId: process.env.TWILIO_NUMBER,
     }, req.body.number);
 
