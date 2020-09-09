ef16dbfb6d7948323c9f393ed409b9d8b3678828
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-09-09 11:33:40 +0300

    added the update and delete agent

--- app/controller/agentController.js
+++ app/controller/agentController.js
@@ -1,27 +1,29 @@
-const models = require('../../database/models')
+const models = require('../../database/models');
 const bcrypt = require('bcrypt');
 
 async function createAgent(req, res) {
     try {
         const agent = req.body;
+        agent.password = await bcrypt.hash(agent.password, 10);
+        agent.states = JSON.stringify(agent.states);
 
         console.log("createAgent -> agent", agent)
 
-        const userExist = await models.Users.findOne({
+        const user_exist = await models.Users.findOne({
             attributes: ['id'],
             where: {
                 email: agent.email
             }
         })
 
-        if (!userExist) {
+        if (!user_exist) {
             await models.Users.create({
                 role_id: 2,
                 fname: agent.fname,
                 lname: agent.lname,
                 email: agent.email,
-                password: await bcrypt.hash(agent.password, 10),
-                states: JSON.stringify(agent.states)
+                password: agent.password,
+                states: agent.states
             });
 
             return res.status(201).json({
@@ -45,36 +47,111 @@ async function createAgent(req, res) {
 }
 
 async function updateAgent(req, res) {
-    await models.Users.update({
-        fname: agent.fname,
-        lname: agent.lname,
-        email: agent.email,
-        states: agent.states
-    }, {
-        where: {
-            id: req.body.id
-        },
-    })
+    try {
+        const agent_candidate = await models.Users.findOne({
+            where: { id: req.params.agent_id }
+        });
+
+        if (agent_candidate) {
+            const agent = req.body;
+            agent.password = await bcrypt.hash(agent.password, 10);
+            agent.states = JSON.stringify(agent.states);
+
+            await agent_candidate.update({
+                fname: agent.fname,
+                lname: agent.lname,
+                email: agent.email,
+                password: agent.password,
+                states: agent.states
+            });
+
+            return res.status(200).json({
+                status: "success",
+                message: 'Agent updated'
+            });
+        } else {
+            return res.status(200).json({
+                status: "failed",
+                message: 'Agent not found'
+            });
+        }
+    } catch (error) {
+        res.status(400).json({
+            status: "failed",
+            message: 'Server error'
+        });
+        throw new Error(error);
+    }
 }
 
 async function deleteAgent(req, res) {
     try {
-        await models.Users.destroy({
-            where: {
-                id: req.body.id
-            }
+        const agent_candidate = await models.Users.findOne({
+            where: { id: req.params.agent_id }
         });
 
-        return res.status(200).json({
-            message: "Agent removed success!"
-        });
+        if (agent_candidate) {
+            await agent_candidate.destroy();
+
+            return res.status(200).json({
+                status: "success",
+                message: "Agent removed success!"
+            });
+        } else {
+            return res.status(200).json({
+                status: "failed",
+                message: "Agent dont't removed"
+            });
+        }
+
     } catch (error) {
-        console.error(error);
+        res.status(400).json({
+            status: "failed",
+            message: "Server error"
+        });
+        throw new Error(error);
     }
+}
 
-    return res.status(400).json({
-        message: "Server error"
-    });
+async function updateAgentPassword(req, res) {
+    try {
+        const agent_candidate = await models.Users.findOne({
+            where: { id: req.params.agent_id }
+        });
+
+        if (agent_candidate) {
+            const password_mathes = await bcrypt.compare(req.body.old_password, agent_candidate.dataValues.password);
+
+            if (password_mathes) {
+                const new_password = await bcrypt.hash(req.body.new_password, 10);
+
+                await agent_candidate.update({
+                    password: new_password
+                });
+
+                return res.status(200).json({
+                    status: 'success',
+                    message: "Password updated!"
+                });
+            } else {
+                return res.status(200).json({
+                    status: 'failed',
+                    message: "Password don't mathes"
+                });
+            }
+        } else {
+            return res.status(200).json({
+                status: 'failed',
+                message: "Agent not exist!"
+            });
+        }
+    } catch (error) {
+        res.status(200).json({
+            status: 'failed',
+            message: "Server error!"
+        });
+        throw new Error(error);
+    }
 }
 
 // TODO CREATE FUNCTION FOR SELF EDITING OF AGENT ACCOUNT, CHANGING PASSWORD|
@@ -82,5 +159,6 @@ async function deleteAgent(req, res) {
 module.exports = {
     createAgent,
     updateAgent,
-    deleteAgent
+    deleteAgent,
+    updateAgentPassword
 }
--- routes/agent.js
+++ routes/agent.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const { authenticateToken } = require('../app/middleware/authMiddleware');
-const { createAgent } = require('../app/controller/agentController');
+const { createAgent, updateAgent, deleteAgent, updateAgentPassword } = require('../app/controller/agentController');
 
 
 router.post('/create', authenticateToken, createAgent);
-// router.post('/update', verify);
-// router.post('/delete', verify);
-
+router.post('/update/:agent_id', updateAgent);
+router.delete('/delete/:agent_id', deleteAgent);
+router.post('/update-password/:agent_id', updateAgentPassword);
 module.exports = router;
