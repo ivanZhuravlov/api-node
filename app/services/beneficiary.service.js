ff8c929035df83b10792181b4a612d1652387b6e
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-09-11 22:15:08 +0300

    Removed #

--- app/services/beneficiary.service.js
+++ app/services/beneficiary.service.js
@@ -2,7 +2,7 @@ const models = require('../../database/models');
 const BeneficiaryRepository = require('../repository/BeneficiaryRepository');
 
 class BeneficiaryService {
-    #state_id = null;
+    state_id = null;
 
     async save(beneficiary_options) {
         try {
@@ -15,14 +15,14 @@ class BeneficiaryService {
                     where: { name: beneficiary_options.location }
                 });
 
-                this.#state_id = state.dataValues.id;
+                this.state_id = state.dataValues.id;
             }
 
             if (beneficiary) {
                 await beneficiary.update({
                     name: beneficiary_options.name,
                     relative_id: beneficiary_options.relative_id,
-                    location_id: this.#state_id,
+                    location_id: this.state_id,
                     grand_kids: beneficiary_options.grand_kids,
                     work_status: beneficiary_options.work_status
                 });
@@ -33,7 +33,7 @@ class BeneficiaryService {
                     lead_id: beneficiary_options.lead_id,
                     name: beneficiary_options.name,
                     relative_id: beneficiary_options.relative_id,
-                    location_id: this.#state_id,
+                    location_id: this.state_id,
                     grand_kids: beneficiary_options.grand_kids,
                     work_status: beneficiary_options.work_status
                 });
