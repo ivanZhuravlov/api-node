4605a497f59003892a4f2bcb146ff6cb8c7ab436
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-09-25 12:53:11 +0300

    fixed issue with missed agent id

--- app/repository/agent.repository.js
+++ app/repository/agent.repository.js
@@ -1,15 +1,17 @@
 const db = require('../../database/models');
 
 const AgentRepository = {
+    /**
+     * Find agent with smallest 
+     * @param {number} state_id 
+     */
     async getAgentWithSmallestCountLeads(state_id) {
         try {
             const data = await db.sequelize.query("SELECT users.id, COUNT(leads.id) AS `count` FROM users INNER JOIN users_states ON users_states.user_id = users.id LEFT JOIN leads ON leads.user_id = users.id WHERE users_states.state_id = '" + state_id + "' GROUP BY users.id ORDER BY `count` ASC LIMIT 1", {
                 type: db.sequelize.QueryTypes.SELECT,
             });
 
-            console.log("getAgentWithSmallestCountLeads -> data[0].id", data[0].id)
-
-            if (data[0]) {
+            if (data) {
                 return data[0].id;
             } else {
                 return null;
@@ -19,13 +21,15 @@ const AgentRepository = {
         }
     },
 
+    /**
+     * Find role
+     * @param {number} user_id 
+     */
     async getRole(user_id) {
         try {
             const data = await db.sequelize.query("SELECT roles.name AS role FROM users INNER JOIN roles ON roles.id = users.role_id WHERE users.id = " + user_id, {
                 type: db.sequelize.QueryTypes.SELECT,
             });
-
-            console.log("getRole -> data", data)
 
             if (data) {
                 return data[0].role
