0c57b628ebe356936243b8f233d1467b254ac965
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-09-25 14:26:20 +0300

    updated records

--- app/services/record.js
+++ app/services/record.js
@@ -1,21 +1,5 @@
-const models = require('../../database/models/index');
 
-/**
- * @param {*} req 
- * @param {*} res 
- */
-async function createRecord(record) {
-    try {
-        await models.Records.create({
-            lead_id: record.leadId,
-            datetime: record.datatime,
-            call_sid: record.callSid,
-            sid: record.sid
-        });
-    } catch (e) {
-        console.error(e);
-    }
-}
+
 
 module.exports = {
     createRecord
