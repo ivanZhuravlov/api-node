485a4aec6d67b82d43754f1299dc06f1ae708b08
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-09-25 12:20:34 +0300

    updated routes

--- app/controller/lead.controller.js
+++ app/controller/lead.controller.js
@@ -79,18 +79,18 @@ async function getCompaniesListByLeadData(req, res) {
 
         const companies = await quotes.getCompaniesInfo();
 
-        if ("email" in rawLead) {
-                const html = MailService.generateQuotesHtmlTemplate('quote.ejs', companies);
+        // if ("email" in rawLead) {
+        //         const html = MailService.generateQuotesHtmlTemplate('quote.ejs', companies);
     
-                const mail_options = {
-                    from: process.env.MAIL_SERVICE_USER_EMAIL,
-                    to: rawLead.email,
-                    subject: "Blueberry Insurance",
-                    html
-                };
+        //         const mail_options = {
+        //             from: process.env.MAIL_SERVICE_USER_EMAIL,
+        //             to: rawLead.email,
+        //             subject: "Blueberry Insurance",
+        //             html
+        //         };
     
-                MailService.send(mail_options);
-        }
+        //         MailService.send(mail_options);
+        // }
 
         return res.status(200).json(companies);
     } catch (err) {
