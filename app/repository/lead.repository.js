c731e68fe5800b401de5b5300e0f607aec11c4a4
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-11-03 19:30:12 +0200

    add new company and fixed benef and optimizated lead repo

--- app/repository/lead.repository.js
+++ app/repository/lead.repository.js
@@ -44,49 +44,21 @@ const LeadRepository = {
     async getOne(id) {
 
         try {
-            const data = await db.sequelize.query("SELECT leads.id, leads.user_id, leads.state_id, leads.phone, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, users.email as agent_email, leads.email, leads.property, leads.busy, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt AS updated, sources.title AS source_title, sources.name AS source FROM leads INNER JOIN sources ON sources.id = leads.source_id LEFT JOIN users ON leads.user_id = users.id LEFT JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id WHERE leads.id = " + id, {
+            let lead = await db.sequelize.query("SELECT leads.id, leads.user_id, leads.state_id, leads.phone, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, users.email as agent_email, leads.email, leads.property, leads.busy, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt AS updated, sources.title AS source_title, sources.name AS source FROM leads INNER JOIN sources ON sources.id = leads.source_id LEFT JOIN users ON leads.user_id = users.id LEFT JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id WHERE leads.id = " + id, {
                 type: db.sequelize.QueryTypes.SELECT,
+                plain: true
             });
 
-            let lead = data[0];
-
             lead.property = JSON.parse(lead.property);
-
             lead.price = JSON.parse(lead.price);
-
             lead = { ...lead, ...lead.property };
-
             lead.updated = TransformationHelper.formatDate(lead.updated, true);
-
             delete lead.property;
 
             return lead;
         } catch (error) {
             throw error;
         }
-        // return new Promise(async (resolve, reject) => {
-        //     const data = await db.sequelize.query("SELECT leads.id, leads.user_id, leads.state_id, leads.phone, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, users.email as agent_email, leads.email, leads.property, leads.busy, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.createdAt AS created, sources.title AS source_title, sources.name AS source FROM leads INNER JOIN sources ON sources.id = leads.source_id LEFT JOIN users ON leads.user_id = users.id LEFT JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id WHERE leads.id = " + id, {
-        //         type: db.sequelize.QueryTypes.SELECT,
-        //     }).catch(e => {
-        //         console.error(e);
-        //     });
-
-        //     if (data) {
-        //         let lead = data[0];
-
-        //         lead.property = JSON.parse(lead.property);
-
-        //         lead.price = JSON.parse(lead.price);
-
-        //         lead = { ...lead, ...lead.property };
-
-        //         lead.created = this.formatDate(lead.created, true);
-
-        //         delete lead.property;
-
-        //         return resolve(lead);
-        //     }
-        // });
     },
 
     async getEmptyAll() {
@@ -118,43 +90,15 @@ const LeadRepository = {
         } catch (error) {
             throw error;
         }
-        // return new Promise(async (resolve, reject) => {
-        //     let data = await db.sequelize.query("SELECT leads.id, leads.createdAt, leads.empty, leads.phone, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, leads.email, leads.property, leads.busy, types.name AS type, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.createdAt AS created FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id LEFT JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id LEFT JOIN types ON leads.type_id = types.id WHERE leads.empty = 1 AND (leads.phone IS NOT NULL OR leads.email IS NOT NULL) ORDER BY leads.id DESC;", {
-        //         type: db.sequelize.QueryTypes.SELECT
-        //     }).catch((e) => {
-        //         console.error(e);
-        //     });
-
-        //     data = data.map(lead => {
-        //         lead.property = JSON.parse(lead.property);
-        //         lead.price = JSON.parse(lead.price);
-
-        //         if (lead.property.birth_date) {
-        //             lead.property.birth_date = this.formatDate(lead.property.birth_date)
-        //         }
-
-        //         lead = { ...lead, ...lead.property };
-
-        //         if ('property' in lead) {
-        //             delete lead.property;
-        //         }
-
-        //         lead.createdAt = this.formatDate(lead.createdAt, true);
-
-        //         return lead;
-        //     });
-
-        //     return resolve(data);
-        // });
     },
 
     async getRawLead(lead_id) {
         try {
-            let data = await db.sequelize.query("SELECT leads.id, leads.createdAt, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, leads.email, leads.property, leads.busy, types.name AS type, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt AS updated FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id INNER JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id LEFT JOIN types ON leads.type_id = types.id WHERE leads.id = " + lead_id, {
-                type: db.sequelize.QueryTypes.SELECT
+            let lead = await db.sequelize.query("SELECT leads.id, leads.createdAt, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, leads.email, leads.property, leads.busy, types.name AS type, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt AS updated FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id INNER JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id LEFT JOIN types ON leads.type_id = types.id WHERE leads.id = " + lead_id, {
+                type: db.sequelize.QueryTypes.SELECT,
+                plain: true
             });
 
-            const lead = data[0];
             lead.property = JSON.parse(lead.property);
             lead.price = JSON.parse(lead.price);
 
@@ -168,27 +112,6 @@ const LeadRepository = {
         } catch (error) {
             throw error;
         }
-        // return new Promise(async (resolve, reject) => {
-        //     let data = await db.sequelize.query("SELECT leads.id, leads.createdAt, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, leads.email, leads.property, leads.busy, types.name AS type, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.createdAt AS created FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id INNER JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id LEFT JOIN types ON leads.type_id = types.id WHERE leads.id = " + lead_id, {
-        //         type: db.sequelize.QueryTypes.SELECT
-        //     }).catch((e) => {
-        //         console.error(e);
-        //     });
-
-        //     if (data) {
-        //         const lead = data[0];
-        //         lead.property = JSON.parse(lead.property);
-        //         lead.price = JSON.parse(lead.price);
-
-        //         if ("birth_date" in lead.property) {
-        //             lead.property.birth_date = this.formatDate(lead.property.birth_date, false);
-        //         }
-
-        //         lead.createdAt = this.formatDate(lead.createdAt, true);
-
-        //         return resolve(lead);
-        //     }
-        // });
     },
 
     async getByUserId(type, user_id) {
@@ -220,35 +143,6 @@ const LeadRepository = {
         } catch (error) {
             throw error;
         }
-        // return new Promise(async (resolve, reject) => {
-        //     let data = await db.sequelize.query("SELECT leads.id, leads.user_id, leads.phone, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, users.email as agent_email, leads.email, leads.property, leads.busy, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.createdAt AS created, sources.title AS source_title, sources.name AS source FROM leads INNER JOIN sources ON sources.id = leads.source_id LEFT JOIN users ON leads.user_id = users.id LEFT JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id WHERE leads.user_id = '" + user_id + "' AND leads.type_id = (SELECT types.id FROM types WHERE types.name = '" + type.toLowerCase() + "') ORDER BY leads.id DESC;", {
-        //         type: db.sequelize.QueryTypes.SELECT,
-        //     }).catch(e => {
-        //         console.error(e);
-        //     });
-
-        //     data = data.map(lead => {
-        //         lead.property = JSON.parse(lead.property);
-
-        //         if ("agent_fullname" in lead.property) {
-        //             delete lead.property.agent_fullname
-        //         }
-
-        //         if ("agent_email" in lead.property) {
-        //             delete lead.property.agent_email
-        //         }
-
-        //         lead.price = JSON.parse(lead.price);
-
-        //         lead.created = this.formatDate(lead.created, true);
-
-        //         lead = { ...lead, ...lead.property };
-
-        //         return lead;
-        //     });
-
-        //     return resolve(data);
-        // });
     },
 
     async getLeadsBySource(source) {
@@ -299,30 +193,6 @@ const LeadRepository = {
             throw error;
         }
     },
-
-    // getLatest(latestId) {
-    //     return new Promise(async (resolve, reject) => {
-    //         let data = await db.sequelize.query(`SELECT leads.id, leads.createdAt, leads.empty, leads.fullname, users.fname, users.lname, leads.email, leads.property, leads.busy, types.name AS type, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.createdAt AS created FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id LEFT JOIN types ON leads.type_id = types.id WHERE leads.id IN ('${latestId.join("', '")}')`, {
-    //             type: db.sequelize.QueryTypes.SELECT
-    //         }).catch((e) => {
-    //             console.error(e);
-    //         });
-
-    //         data = data.map(item => {
-    //             item.property = JSON.parse(item.property);
-
-    //             if (item.property.birth_date) {
-    //                 item.property.birth_date = this.formatDate(item.property.birth_date)
-    //             }
-
-    //             item.createdAt = this.formatDate(item.createdAt, true);
-
-    //             return item;
-    //         });
-
-    //         return resolve(data);
-    //     });
-    // },
 }
 
 module.exports = LeadRepository;
