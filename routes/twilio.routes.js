306b8074ea4ac88f8567ed67525b5f5e7e8b0a7a
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-11-16 15:11:12 +0200

    Removed AMD and added call transfere dirrectly

--- routes/twilio.routes.js
+++ routes/twilio.routes.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const TwilioController = require('../app/controller/twilio.controller');
 
 router.post('/outbound-call', TwilioController.outboundCall);
-router.post('/amd', TwilioController.AMD);
+// router.post('/amd', TwilioController.AMD);
+router.post('/answered', TwilioController.answeredCallBack);
 router.post('/token/:workerId', TwilioController.token);
 router.post('/conference/:conferenceId/connect/:id', TwilioController.connectToConference);
 router.post('/addworker', TwilioController.addWorkerToTheCall);
