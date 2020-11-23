f53b3ef4a2eaf3823202c8916e9ad61c87ccb76b
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-11-23 16:54:19 +0200

    Merge branch 'twilio' of gitlab.com:blueberry-insurance/api-node into twilio

--- app/controller/agent.controller.js
+++ app/controller/agent.controller.js
@@ -191,6 +191,16 @@ async function deleteScript(req, res) {
     }
 }
 
+async function getOnlineAgents(req, res) {
+    try {
+        const agents = await AgentFacade.getOnlineAgents();
+        res.status(200).json({ status: 'success', agents });
+    } catch (error) {
+        res.status(500).json({ status: 'error', message: 'Server Error' });
+        throw error;
+    }
+}
+
 module.exports = {
     getAgents,
     createAgent,
@@ -203,5 +213,6 @@ module.exports = {
     createScript,
     getAllScripts,
     deleteScript,
-    updateScript
+    updateScript,
+    getOnlineAgents
 }
