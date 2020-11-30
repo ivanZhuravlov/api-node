10394b01a26a0c3bf80e87a97600ab57094ab924
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-11-30 14:12:05 +0200

    deleted format date for updated field

--- app/repository/lead.repository.js
+++ app/repository/lead.repository.js
@@ -4,14 +4,8 @@ const TransformationHelper = require('../helpers/transformation.helper');
 const LeadRepository = {
     async getGuideLeads() {
         try {
-            let data = await db.sequelize.query(`SELECT *, sources.title as source, leads.id as id, status.title as status, states.title as state, leads.updatedAt FROM leads INNER JOIN status ON status.id = leads.status_id LEFT JOIN sources ON sources.id = leads.source_id LEFT JOIN states ON states.id = leads.state_id WHERE leads.source_id IN (2, 4) AND leads.status_id IN (1, 2, 3, 4, 5, 11) AND leads.phone IS NOT NULL ORDER BY leads.id DESC;`, {
+            let data = await db.sequelize.query(`SELECT *, sources.title as source, leads.id as id, status.title as status, states.title as state, leads.updatedAt AS updated FROM leads INNER JOIN status ON status.id = leads.status_id LEFT JOIN sources ON sources.id = leads.source_id LEFT JOIN states ON states.id = leads.state_id WHERE leads.source_id IN (2, 4) AND leads.status_id IN (1, 2, 3, 4, 5, 11) AND leads.phone IS NOT NULL ORDER BY leads.id DESC;`, {
                 type: db.sequelize.QueryTypes.SELECT,
-            });
-
-            data = data.map(lead => {
-                lead.updated = TransformationHelper.formatDate(lead.updatedAt, true);
-                delete lead.updatedAt;
-                return lead;
             });
 
             return data;
@@ -36,7 +30,7 @@ const LeadRepository = {
                 statesQuery = `and leads.state_id in (${stringOfStatesIds})`;
             }
 
-            let data = await db.sequelize.query(`SELECT leads.id, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, users.email as agent_email, leads.email, leads.property, leads.busy, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id INNER JOIN status ON leads.status_id = status.id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id WHERE leads.empty = 0 AND leads.type_id = (SELECT types.id FROM types WHERE types.name = '${type}') ${statesQuery} ORDER BY leads.id DESC;`, {
+            let data = await db.sequelize.query(`SELECT leads.id, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, users.email as agent_email, leads.email, leads.property, leads.busy, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt AS updated FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id INNER JOIN status ON leads.status_id = status.id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id WHERE leads.empty = 0 AND leads.type_id = (SELECT types.id FROM types WHERE types.name = '${type}') ${statesQuery} ORDER BY leads.id DESC;`, {
                 type: db.sequelize.QueryTypes.SELECT,
             }).catch((e) => {
                 console.error(e);
@@ -46,9 +40,6 @@ const LeadRepository = {
                 lead.property = JSON.parse(lead.property);
                 lead.price = JSON.parse(lead.price);
                 lead = { ...lead, ...lead.property };
-
-                lead.updated = TransformationHelper.formatDate(lead.updatedAt, true);
-                delete lead.updatedAt;
 
                 return lead;
             });
@@ -61,7 +52,7 @@ const LeadRepository = {
 
     async getOne(id) {
         try {
-            let lead = await db.sequelize.query("SELECT leads.id, leads.user_id, leads.state_id, leads.phone, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, users.email as agent_email, leads.email, leads.property, leads.busy, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt, sources.title AS source_title, sources.name AS source FROM leads INNER JOIN sources ON sources.id = leads.source_id LEFT JOIN users ON leads.user_id = users.id LEFT JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id WHERE leads.id = " + id, {
+            let lead = await db.sequelize.query("SELECT leads.id, leads.user_id, leads.state_id, leads.phone, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, users.email as agent_email, leads.email, leads.property, leads.busy, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt AS updated, sources.title AS source_title, sources.name AS source FROM leads INNER JOIN sources ON sources.id = leads.source_id LEFT JOIN users ON leads.user_id = users.id LEFT JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id WHERE leads.id = " + id, {
                 type: db.sequelize.QueryTypes.SELECT,
                 plain: true
             });
@@ -69,8 +60,6 @@ const LeadRepository = {
             lead.property = JSON.parse(lead.property);
             lead.price = JSON.parse(lead.price);
             lead = { ...lead, ...lead.property };
-            lead.updated = TransformationHelper.formatDate(lead.updatedAt, true);
-            delete lead.updatedAt;
 
             return lead;
         } catch (error) {
@@ -80,7 +69,7 @@ const LeadRepository = {
 
     async getEmptyAll() {
         try {
-            let data = await db.sequelize.query("SELECT leads.id, leads.createdAt, leads.empty, leads.phone, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, leads.email, leads.property, leads.busy, types.name AS type, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id LEFT JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id LEFT JOIN types ON leads.type_id = types.id WHERE leads.empty = 1 AND (leads.phone IS NOT NULL OR leads.email IS NOT NULL) ORDER BY leads.id DESC;", {
+            let data = await db.sequelize.query("SELECT leads.id, leads.createdAt, leads.empty, leads.phone, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, leads.email, leads.property, leads.busy, types.name AS type, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt AS updated FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id LEFT JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id LEFT JOIN types ON leads.type_id = types.id WHERE leads.empty = 1 AND (leads.phone IS NOT NULL OR leads.email IS NOT NULL) ORDER BY leads.id DESC;", {
                 type: db.sequelize.QueryTypes.SELECT
             });
 
@@ -96,9 +85,6 @@ const LeadRepository = {
 
                 if ('property' in lead) delete lead.property;
 
-                lead.updated = TransformationHelper.formatDate(lead.updatedAt, true);
-                delete lead.updatedAt;
-
                 return lead;
             });
 
@@ -110,7 +96,7 @@ const LeadRepository = {
 
     async getRawLead(lead_id) {
         try {
-            let lead = await db.sequelize.query("SELECT leads.id, leads.createdAt, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, leads.email, leads.property, leads.busy, types.name AS type, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id INNER JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id LEFT JOIN types ON leads.type_id = types.id WHERE leads.id = " + lead_id, {
+            let lead = await db.sequelize.query("SELECT leads.id, leads.createdAt, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, leads.email, leads.property, leads.busy, types.name AS type, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt AS updated FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id INNER JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id LEFT JOIN types ON leads.type_id = types.id WHERE leads.id = " + lead_id, {
                 type: db.sequelize.QueryTypes.SELECT,
                 plain: true
             });
@@ -122,9 +108,6 @@ const LeadRepository = {
                 lead.property.birth_date = TransformationHelper.formatDate(lead.property.birth_date, false);
             }
 
-            lead.updated = TransformationHelper.formatDate(lead.updatedAt, true);
-            delete lead.updatedAt;
-
             return lead;
         } catch (error) {
             throw error;
@@ -133,7 +116,7 @@ const LeadRepository = {
 
     async getByUserId(type, user_id) {
         try {
-            let data = await db.sequelize.query("SELECT leads.id, leads.user_id, leads.phone, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, users.email as agent_email, leads.email, leads.property, leads.busy, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt, sources.title AS source_title, sources.name AS source FROM leads INNER JOIN sources ON sources.id = leads.source_id LEFT JOIN users ON leads.user_id = users.id LEFT JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id WHERE leads.user_id = '" + user_id + "' AND leads.type_id = (SELECT types.id FROM types WHERE types.name = '" + type.toLowerCase() + "') ORDER BY leads.id DESC;", {
+            let data = await db.sequelize.query("SELECT leads.id, leads.user_id, leads.phone, leads.empty, leads.fullname, CONCAT(users.fname, ' ', users.lname) as agent_fullname, users.email as agent_email, leads.email, leads.property, leads.busy, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt AS updated, sources.title AS source_title, sources.name AS source FROM leads INNER JOIN sources ON sources.id = leads.source_id LEFT JOIN users ON leads.user_id = users.id LEFT JOIN status ON leads.status_id = status.id LEFT JOIN states ON leads.state_id = states.id LEFT JOIN prices ON leads.id = prices.lead_id WHERE leads.user_id = '" + user_id + "' AND leads.type_id = (SELECT types.id FROM types WHERE types.name = '" + type.toLowerCase() + "') ORDER BY leads.id DESC;", {
                 type: db.sequelize.QueryTypes.SELECT,
             })
             data = data.map(lead => {
@@ -150,9 +133,6 @@ const LeadRepository = {
                 lead.price = JSON.parse(lead.price);
                 lead = { ...lead, ...lead.property };
 
-                lead.updated = TransformationHelper.formatDate(lead.updatedAt, true);
-                delete lead.updatedAt;
-
                 return lead;
             });
 
@@ -164,7 +144,7 @@ const LeadRepository = {
 
     async getLeadsBySource(source) {
         try {
-            let data = await db.sequelize.query(`SELECT leads.id, leads.empty, leads.fullname, users.fname, users.lname, users.email as agent_email, CONCAT(users.fname, ' ', users.lname) as agent_fullname, leads.email, leads.property, leads.busy, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id INNER JOIN status ON leads.status_id = status.id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id WHERE leads.empty = 0 AND sources.name = "${source}" ORDER BY leads.id DESC;`, {
+            let data = await db.sequelize.query(`SELECT leads.id, leads.empty, leads.fullname, users.fname, users.lname, users.email as agent_email, CONCAT(users.fname, ' ', users.lname) as agent_fullname, leads.email, leads.property, leads.busy, sources.title AS source_title, sources.name AS source, status.name AS status, status.title AS status_title, states.name AS state, prices.price, leads.updatedAt AS updated FROM leads LEFT JOIN users ON leads.user_id = users.id INNER JOIN sources ON leads.source_id = sources.id INNER JOIN status ON leads.status_id = status.id INNER JOIN states ON leads.state_id = states.id INNER JOIN prices ON leads.id = prices.lead_id WHERE leads.empty = 0 AND sources.name = "${source}" ORDER BY leads.id DESC;`, {
                 type: db.sequelize.QueryTypes.SELECT,
             });
 
@@ -173,9 +153,6 @@ const LeadRepository = {
                 lead.price = JSON.parse(lead.price);
 
                 lead = { ...lead, ...lead.property };
-
-                lead.updated = TransformationHelper.formatDate(lead.updatedAt, true);
-                delete lead.updatedAt;
 
                 return lead;
             });
