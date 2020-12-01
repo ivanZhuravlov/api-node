a4a8db19ead360659426e87ebc37666eb9e69e10
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2020-12-01 23:02:20 +0200

    fixed issue with showing leads

--- app/repository/lead.repository.js
+++ app/repository/lead.repository.js
@@ -4,7 +4,8 @@ const TransformationHelper = require('../helpers/transformation.helper');
 const LeadRepository = {
     async getGuideLeads() {
         try {
-            let data = await db.sequelize.query(`SELECT *, sources.title as source, leads.id as id, status.title as status, states.title as state, leads.updatedAt AS updated FROM leads INNER JOIN status ON status.id = leads.status_id LEFT JOIN sources ON sources.id = leads.source_id LEFT JOIN states ON states.id = leads.state_id WHERE leads.source_id IN (2, 4) AND leads.status_id IN (1, 2, 3, 4, 5, 11) AND leads.phone IS NOT NULL ORDER BY leads.id DESC;`, {
+            // leads.source_id IN (2, 4) AND
+            let data = await db.sequelize.query(`SELECT *, sources.title as source, leads.id as id, status.title as status, states.title as state, leads.updatedAt AS updated FROM leads INNER JOIN status ON status.id = leads.status_id LEFT JOIN sources ON sources.id = leads.source_id LEFT JOIN states ON states.id = leads.state_id WHERE leads.status_id IN (1, 2, 3, 4, 5, 11) AND leads.phone IS NOT NULL ORDER BY leads.id DESC;`, {
                 type: db.sequelize.QueryTypes.SELECT,
             });
 
