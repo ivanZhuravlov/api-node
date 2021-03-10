d690a89a4e2f85c0af80333ebae31ca18b57ace3
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2021-03-10 18:47:57 +0200

    fixed issue

--- app/repository/statistic.repository.js
+++ app/repository/statistic.repository.js
@@ -7,7 +7,6 @@ class StatisticRepository {
             let sourcesQuery = filters.sources.length ? ' AND l.source_id IN (' + filters.sources.join(', ') + ')' : " AND l.source_id = 0 ";
             let agentsQuery = filters.agents.length ? ' AND u.id IN (' + filters.agents.join(', ') + ')' : " AND l.id = 0 ";
 
-
             const startEnd = () => {
                 let query = '';
 
@@ -25,8 +24,6 @@ class StatisticRepository {
             const statInfo = await db.sequelize.query(`SELECT CONCAT(u.fname, ' ', u.lname) agent, COUNT(l.id) c_a, (SELECT COUNT(l.id) FROM leads l WHERE l.user_id = u.id AND l.status_id IN (20, 21, 22, 15, 16)${sourcesQuery}${typeQuery}${startEnd()}) c_d, (SELECT COUNT(l.id) FROM leads l WHERE l.user_id = u.id AND l.status_id IN (20, 21, 22, 15, 16)${sourcesQuery}${typeQuery}${startEnd()})/COUNT(l.id)*100 c_r  FROM users u INNER JOIN leads l ON l.user_id = u.id WHERE u.role_id = 2${agentsQuery}${sourcesQuery}${typeQuery}${startEnd()} GROUP BY u.fname ORDER BY u.fname`, {
                 type: db.sequelize.QueryTypes.SELECT
             });
-
-            console.log(statInfo);
 
             return statInfo;
         } catch (error) {
