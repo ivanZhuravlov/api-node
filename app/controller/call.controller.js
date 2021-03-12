8e8339b7a06fae9001ce4a679eeebd22a0bb8ce9
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2021-03-12 16:42:59 +0200

    updated twilio token ttl

--- app/controller/call.controller.js
+++ app/controller/call.controller.js
@@ -16,7 +16,8 @@ class CallController {
     token(req, res) {
         const capability = new ClientCapability({
             accountSid: process.env.TWILIO_ACCOUNT_SID,
-            authToken: process.env.TWILIO_AUTH_TOKEN
+            authToken: process.env.TWILIO_AUTH_TOKEN,
+            ttl: 43200
         });
 
         capability.addScope(
