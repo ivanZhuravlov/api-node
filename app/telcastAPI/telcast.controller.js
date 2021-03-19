cb37375a5d436d627accf8c06951cb3d0e5d54c3
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2021-03-19 13:25:20 +0200

    uploading leads to telecast

--- app/telcastAPI/telcast.controller.js
+++ app/telcastAPI/telcast.controller.js
@@ -1,12 +1,35 @@
 const TelcastService = require('./telcast.service');
-
+const LeadRepository = require('../repository/lead.repository');
 class TelcastController {
-    index(req, res){
+    /**
+     * Upload list count of leads to TelcastAPI
+     * @param {*} req 
+     * @param {*} res 
+     */
+    async index(req, res) {
         try {
-            // find the list of leads
-            TelcastService.request();
+            if (req.body) {
 
+                const data = req.body;
+
+                let leads;
+
+                if (data.filters) {
+                    leads = await LeadRepository.getLeadsByFilters(data.filters);
+                } else if (data.leads) {
+                    leads = data.leads;
+                }
+
+                leads.forEach(lead => {
+                    TelcastService.sendLead(lead);
+                });
+
+                return res.status(200).send({ status: "success", message: "Leads sent!" });
+            }
+
+            return res.status(400).send({ status: "error", message: "Bad request!" });
         } catch (error) {
+            return res.status(500).send({ status: "error", message: "Server error!" });
             throw error;
         }
     }
--- app/telcastAPI/telcast.service.js
+++ app/telcastAPI/telcast.service.js
@@ -1,26 +1,72 @@
-const fetch = require('node-fetch');
 const request = require('request');
 const winston = require('winston');
+
+/**
+ * Logged with params
+ */
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
     transports: [
-        new winston.transports.File({ filename: '../../logs/telcast/err_telcast.log', level: 'error' }),
-        new winston.transports.File({ filename: '../../logs/telcast/inf_telcast.log' }),
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'logs/telcast.log' })
     ],
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(info => {
+            return `${info.timestamp} ${info.id} : "${info.message.trimEnd()}"`;
+        })
+    ),
 });
 
 class TelcastService {
-    request(lead) {
+    sendLead(lead) {
         try {
             const url = process.env.TELCAST_API_URL;
-            const params = { a: 1 };
+
+            let list_id;
+
+            if (lead.type_id == 2) {
+                list_id = 1004;
+            } else if (lead.type_id == 4) {
+                list_id = 1005;
+            }
+
+            const params = {
+                source: "api",
+                user: process.env.TELCAST_API_USER,
+                pass: process.env.TELCAST_API_PASS,
+                function: "add_lead",
+                phone_number: lead.phone,
+                list_id: list_id,
+                custom_fields: "Y",
+                lead_status: "Contact Attempt",
+                LeadType: lead.source_title,
+                lead_status: lead.status_title,
+                duplicate_check: "DUPLIST"
+            };
+
+            if (lead.fullname) {
+                const fullname = lead.fullname.split(" ");
+                params.first_name = fullname[0];
+                params.last_name = fullname[1];
+            }
+
+            lead.address ? params.address1 = lead.address : false;
+            lead.city ? params.city = lead.city : false;
+            lead.state ? params.state = lead.state : false;
+            lead.zipcode ? params.postal_code = lead.zipcode : false;
+            lead.gender ? params.gender = lead.gender : false;
+            lead.birth_date ? params.date_of_birth = lead.birth_date : false;
+            lead.email ? params.email = lead.email : false;
 
             request({ url: url, qs: params }, function (err, response, body) {
-                if (err) { console.log(err); return; }
-                console.log("🚀 ~ file: telcast.service.js ~ line 12 ~ TelcastService ~ body", body)
+                if (err) {
+                    logger.log({ level: "error", id: lead.id, message: body });
+                    return;
+                }
+
+                logger.log({ level: "info", id: lead.id, message: body });
             });
+
             return 0;
         } catch (error) {
             throw error;
