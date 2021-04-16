5130c6639dbf0edfae7023ad9d6da8ac08b35e6e
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2021-04-16 19:52:51 +0300

    fix

--- app/twilio/conference/conference.controller.js
+++ app/twilio/conference/conference.controller.js
@@ -71,9 +71,9 @@ class ConferenceController {
         try {
             if (req.body.CallStatus != 'answered') {
                 client.emit("send-second-part-params", false);
-                return res.status(200)
+                return res.status(200).send({});
             }
-            return res.status(400);
+            return res.status(400).send({});
         } catch (error) {
             throw error;
         }
